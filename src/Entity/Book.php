da276de7d9527804a23e7c5e3f60f3e17455893f
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2022-12-12 17:40:16 +0100

    Book entity

---
+++ src/Entity/Book.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::ARRAY, nullable: true)]
+    #[ApiFilter(SearchFilter::class, strategy: 'exact')]
+    private array $category = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCategory(): array
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?array $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
