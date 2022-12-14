e0e77cd3be84badd4fc44424ff31ced26eb6b50e
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2022-12-14 21:33:03 +0100

    persist book item

--- src/State/BookPostProcessor.php
+++ src/State/BookPostProcessor.php
@@ -5,15 +5,27 @@ namespace App\State;
 use App\Entity\BlogPost;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BookPostProcessor implements ProcessorInterface
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    ) {
+    }
+
+
     /**
      * {@inheritDoc}
      */
     public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
     {
-        // call your persistence layer to save $data
+        // "hydra:description": "Unable to generate an IRI for the item of type \"App\\Entity\\Book\""
+        // This was happening because the entity was never being saved, so the id was never generated.
+        // https://stackoverflow.com/questions/57887026/unable-to-generate-an-iri-for-the-item-of-type-exception-after-api-platform-mi
+
+        $this->entityManager->persist($data);
+
         return $data;
     }
 }
